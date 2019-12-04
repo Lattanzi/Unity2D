28e5f0efc7469941e9e34ecd3d383971e08447ce
Lattanzi <lucas_latta@hotmail.com>
2019-12-04 17:46:55 -0300

    Scripts Memory Game
    C# scripts

---
+++ GameManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Sprite[] cardFace;
+    public Sprite cardBack;
+    public GameObject[] Cards;
+    public Text matchText;
+
+    private bool _init = false;
+    private int _matches = 4;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_init)
+        {
+            initializeCards();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            checkCards();
+        }
+    }
+
+    void initializeCards()
+    {
+        for (int id = 0; id < 2; id++)
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                bool test = false;
+                int choice = 0;
+                while (!test)
+                {
+                    choice = Random.Range(0, Cards.Length);
+                    test = !(Cards[choice].GetComponent<card>().initialized);
+                }
+                Cards[choice].GetComponent<card>().cardValue = i;
+                Cards[choice].GetComponent<card>().initialized = true;
+            }
+        }
+        foreach (GameObject c in Cards)
+            c.GetComponent<card>().setupGraphics();
+        if (!_init)
+            _init = true;
+    }
+
+    public Sprite getCardBack()
+    {
+        return cardBack;
+    }
+
+    public Sprite getCardFace(int i)
+    {
+        return cardFace[i - 1];
+    }
+
+    void checkCards()
+    {
+        List<int> c = new List<int>();
+
+        for (int i = 0; i < Cards.Length; i++)
+        {
+            if (Cards[i].GetComponent<card>().state == 1)
+            {
+                c.Add(i);
+            }
+        }
+
+        if (c.Count == 2)
+        {
+            cardComparison(c);
+        }
+    }
+
+    void cardComparison(List<int> c)
+    {
+        card.DO_NOT = true;
+
+        int x = 0;
+
+        if (Cards[c[0]].GetComponent<card> ().cardValue == Cards[c[1]].GetComponent<card>().cardValue)
+        {
+            x = 2;
+            _matches--;
+            matchText.text = "Número de combinações restantes: " + _matches;
+            if (_matches == 0)
+            {
+                SceneManager.LoadScene("Menu");
+            }
+        }
+        for (int i = 0; i < c.Count; i++)
+        {
+            Cards[c[i]].GetComponent<card>().state = x;
+            Cards[c[i]].GetComponent<card>().falseCheck();
+        }
+    }
+}
---
+++ card.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class card : MonoBehaviour
+{
+    public static bool DO_NOT = false;
+
+    [SerializeField]
+    private int _state;
+    [SerializeField]
+    private int _cardValue;
+    [SerializeField]
+    private bool _initialized = false;
+
+    private Sprite cardBack;
+    private Sprite cardFace;
+
+    private GameObject _manager;
+
+    private void Start()
+    {
+        _state = 1;
+        _manager = GameObject.FindGameObjectWithTag("Manager");
+    }
+
+    public void setupGraphics()
+    {
+        cardBack = _manager.GetComponent<GameManager>().getCardBack();
+        cardFace = _manager.GetComponent<GameManager>().getCardFace(_cardValue);
+
+        flipCard();
+    }
+
+    public void flipCard()
+    {
+        if (_state == 0)
+        {
+            _state = 1;
+        }
+        else if (_state == 1)
+        {
+            _state = 0;
+        }
+
+        if (_state == 0 && !DO_NOT)
+        {
+            GetComponent<Image>().sprite = cardBack;
+        }
+        else if (_state == 1 && !DO_NOT)
+        {
+            GetComponent<Image>().sprite = cardFace;
+        }
+    }
+
+    public int cardValue
+    {
+        get
+        {
+            return _cardValue;
+        }
+        set
+        {
+            _cardValue = value;
+        }
+    }
+
+    public int state
+    {
+        get
+        {
+            return _state;
+        }
+        set
+        {
+            _state = value;
+        }
+    }
+
+    public bool initialized
+    {
+        get
+        {
+            return _initialized;
+        }
+        set
+        {
+            _initialized = value;
+        }
+    }
+
+    public void falseCheck()
+    {
+        StartCoroutine ( pause ());
+    }
+
+    IEnumerator pause()
+    {
+        yield return new WaitForSeconds(1);
+        if (_state == 0)
+        {
+            GetComponent<Image>().sprite = cardBack;
+        }
+        else if (_state == 1)
+        {
+            GetComponent<Image>().sprite = cardFace;
+        }
+        DO_NOT = false;
+    }
+}
